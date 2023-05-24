9287d62ab9bdf0ebb40cff24748695c92a950123
Oumar <foumar404@gmail.com>
2023-05-24 21:28:55 +0200

    wip ..

--- variables_rust/src/main.rs
+++ variables_rust/src/main.rs
@@ -18,18 +18,20 @@ fn main() {
 
 //    println!("the value of y is: {y}");
 
-   variable_exemple();
+//    variable_exemple();
 
-   let x:u32 = 10;
-   let y:u32 = 20;
+//    let x:u32 = 10;
+//    let y:u32 = 20;
 
-   println!("Calc: {}", cal_rect(x, y));
-   data_type();
-   floating_point_type();
+//    println!("Calc: {}", cal_rect(x, y));
+//    data_type();
+//    floating_point_type();
+//    numeric_operations();
+   the_character_type();
 }
 
 fn variable_exemple(){
-    let mut x = 5;
+    let mut x: i32 = 5;
     println!("le resultat est: {x}");
 
     x = 7;
@@ -53,4 +55,43 @@ fn floating_point_type(){
     let y: f32 = 3.0; //
 
     println!("la valeur de x et y : {x} {y}");
+}
+
+fn numeric_operations(){
+    // addition 
+    let sum = 5 + 10;
+
+    println!("sum: {sum}");
+
+    //subtraction
+    let difference =  95.5 - 4.3;
+    println!("difference: {difference}");
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    println!("quotient: {quotient}");
+    println!("truncated {truncated}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("remainder: {remainder}");
+
+
+
+}
+
+fn the_boolean_type(){
+    let t = true;
+    let f: bool = false;
+}
+
+fn the_character_type(){
+    let c = 'z';
+    println!("c: {c}");
+    let z: char = 'Z';
+    println!("z: {z}");
+    let heat_eyed_cat = '😻';
+    println!("heat_eyed_cat: {heat_eyed_cat}");
 }
