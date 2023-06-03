652d7ddd837adc1c148f704ad2da0baa79040e95
Oumar <foumar404@gmail.com>
2023-06-03 22:01:25 +0200

    add env

---
+++ interacting_with_cargo/target/debug/deps/interacting_with_cargo-180b27599b8dda93.d
@@ -0,0 +1,5 @@
+c:\Users\oumar\OneDrive\Bureau\dev\omar\#30dayofrust\interacting_with_cargo\target\debug\deps\interacting_with_cargo-180b27599b8dda93.rmeta: src\main.rs
+
+c:\Users\oumar\OneDrive\Bureau\dev\omar\#30dayofrust\interacting_with_cargo\target\debug\deps\interacting_with_cargo-180b27599b8dda93.d: src\main.rs
+
+src\main.rs:
---
+++ interacting_with_cargo/target/debug/deps/interacting_with_cargo-8df2789fdde4575a.d
@@ -0,0 +1,5 @@
+c:\Users\oumar\OneDrive\Bureau\dev\omar\#30dayofrust\interacting_with_cargo\target\debug\deps\interacting_with_cargo-8df2789fdde4575a.rmeta: src\main.rs
+
+c:\Users\oumar\OneDrive\Bureau\dev\omar\#30dayofrust\interacting_with_cargo\target\debug\deps\interacting_with_cargo-8df2789fdde4575a.d: src\main.rs
+
+src\main.rs:
