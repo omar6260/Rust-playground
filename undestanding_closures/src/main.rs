aef19117da626cb81be5f70fed7dd1f7424de867
Oumar <foumar404@gmail.com>
2023-06-08 18:42:38 +0200

    add understanding closure

---
+++ undestanding_closures/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    basic_closure();
+    let one = 2;
+    let closure = move |int_input|{
+        return int_input * one
+    };
+
+    let outcome = add_doubles(Box::new(closure), 2,3);
+    println!("{}", outcome)
+
+}
+
+fn basic_closure(){
+    let test_closure = |string_input|{
+        println!("{}", string_input);
+    };
+    test_closure("test");
+}
+
+fn add_doubles(closure: Box<dyn Fn(i32) -> i32>,
+                one: i32, two:i32) -> i32{
+    return closure(one) + closure(two)
+    
+}
